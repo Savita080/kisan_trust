84f74ee4c0cc089466c259d3c763919bfa43728e
Savita080 <svtdkd@gmail.com>
2026-04-22 14:05:47 +0530

    changed cursor speed

--- frontend/src/app/components/CustomCursor.tsx
+++ frontend/src/app/components/CustomCursor.tsx
@@ -60,7 +60,7 @@ export default function CustomCursor() {
           x: mousePosition.x,
           y: mousePosition.y,
         }}
-        transition={{ type: "spring", stiffness: 400, damping: 28, mass: 0.5 }}
+        transition={{ type: "spring", stiffness: 800, damping: 45, mass: 0.2 }}
       />
       
       {/* The solid center dot */}
@@ -78,7 +78,7 @@ export default function CustomCursor() {
           x: mousePosition.x,
           y: mousePosition.y,
         }}
-        transition={{ type: "spring", stiffness: 1000, damping: 50, mass: 0.1 }}
+        transition={{ type: "spring", stiffness: 2500, damping: 100, mass: 0.05 }}
       />
     </>
   );
